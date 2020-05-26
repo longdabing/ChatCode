ced0e82456dd09cfd2fb57186c6ed1aed8530d99
longdb <576615843@qq.com>
2020-05-26 21:53:38 +0800

    购物车页面实现。

--- pages/fgood/fgood.js
+++ pages/fgood/fgood.js
@@ -5,7 +5,49 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    fgcount:1,
+    sumamount:0,
+    classimglist: [{
+      imgsrc: "/images/fruit01.jpg",
+      classname: "水果01",
+      price:12
+    },
+    {
+      imgsrc: "/images/fruit02.jpg",
+      classname: "水果02",
+      price:14
+    },
+    {
+      imgsrc: "/images/fruit03.jpg",
+      classname: "水果03",
+      price:11
+    },
+    {
+      imgsrc: "/images/fruit04.jpeg",
+      classname: "水果04",
+      price:18
+    },
+    {
+      imgsrc: "/images/fruit01.jpg",
+      classname: "水果05",
+      price:13
+    },
+    {
+      imgsrc: "/images/fruit02.jpg",
+      classname: "水果06",
+      price:16
+    },
+    {
+      imgsrc: "/images/fruit03.jpg",
+      classname: "水果07",
+      price:12
+    },
+    {
+      imgsrc: "/images/fruit04.jpeg",
+      classname: "水果08",
+      price:18
+    }
+  ]
   },
 
   /**
