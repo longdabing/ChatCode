b5bf2175878b8ea40494b1127da2c90a36e05aa1
longdb <576615843@qq.com>
2020-05-27 21:21:08 +0800

    购物车，单选和全选计算总金额。

--- pages/fgood/fgood.js
+++ pages/fgood/fgood.js
@@ -5,51 +5,94 @@ Page({
    * 页面的初始数据
    */
   data: {
-    fgcount:1,
-    sumamount:0,
+    fgcount: 1,
+    sumamount: 0,
+    allcheckvalue: false,
     classimglist: [{
-      imgsrc: "/images/fruit01.jpg",
-      classname: "水果01",
-      price:12
-    },
-    {
-      imgsrc: "/images/fruit02.jpg",
-      classname: "水果02",
-      price:14
-    },
-    {
-      imgsrc: "/images/fruit03.jpg",
-      classname: "水果03",
-      price:11
-    },
-    {
-      imgsrc: "/images/fruit04.jpeg",
-      classname: "水果04",
-      price:18
-    },
-    {
-      imgsrc: "/images/fruit01.jpg",
-      classname: "水果05",
-      price:13
-    },
-    {
-      imgsrc: "/images/fruit02.jpg",
-      classname: "水果06",
-      price:16
-    },
-    {
-      imgsrc: "/images/fruit03.jpg",
-      classname: "水果07",
-      price:12
-    },
-    {
-      imgsrc: "/images/fruit04.jpeg",
-      classname: "水果08",
-      price:18
-    }
-  ]
-  },
+        imgsrc: "/images/fruit01.jpg",
+        classname: "水果01",
+        price: 12,
+        checkvalue: false
+      },
+      {
+        imgsrc: "/images/fruit02.jpg",
+        classname: "水果02",
+        price: 14,
+        checkvalue: false
+      },
+      {
+        imgsrc: "/images/fruit03.jpg",
+        classname: "水果03",
+        price: 11,
+        checkvalue: false
+      },
+      {
+        imgsrc: "/images/bever03.jpg",
+        classname: "饮料03",
+        price: 18,
+        checkvalue: false
+      }
 
+    ]
+  },
+  //单选
+  leftchage: function (e) {
+    //check-group中自定义data-checkindex.
+    var chindex = e.currentTarget.dataset.checkindex;
+    var ischeck = e.detail.value[0];
+
+    if (undefined !== chindex) {
+      var checkprice = this.data.classimglist[chindex].price;
+      var samount = this.data.sumamount;
+      if (undefined !== ischeck) {
+        samount += checkprice;
+      } else {
+        samount -= checkprice;
+      }
+ 
+      this.setData({
+        sumamount: samount
+      })
+      console.log(samount,this.data.allcheckvalue);
+      if (samount == 0) {
+        this.setData({
+          allcheckvalue: false
+        })
+      }
+    }
+  },
+  // check all item
+  allcheckchange: function (e) {
+    var samount = this.data.sumamount;
+    for (var i = 0; i < this.data.classimglist.length; i++) {
+      // console.log(this.data.classimglist[i])
+      console.log(e.detail);
+      let chvalue = 'classimglist[' + i + '].checkvalue';
+      if (this.data.classimglist[i].checkvalue === false) {
+        samount += this.data.classimglist[i].price;
+        console.log(chvalue)
+        this.setData({
+          [chvalue]: true,
+          sumamount: samount,
+          allcheckvalue:true
+        })
+      } else {
+        samount -= this.data.classimglist[i].price;
+        samount = samount <= 0 ? 0 : samount;
+        if (samount === 0 && this.data.allcheckvalue === true) {
+          this.setData({
+            allcheckvalue: false,
+            allcheckvalue:false
+          })
+        }
+        console.log(chvalue)
+        this.setData({
+          [chvalue]: false,
+          sumamount: samount
+        })
+      }
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
