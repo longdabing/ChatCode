34b647c19c59175e486322fc2c1823ab425a5722
longdb <576615843@qq.com>
2020-05-29 21:56:22 +0800

    水果组件页面调整，购物车数量加减计算金额。

--- pages/fgood/fgood.js
+++ pages/fgood/fgood.js
@@ -5,32 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-    fgcount: 1,
     sumamount: 0,
     allcheckvalue: false,
     classimglist: [{
         imgsrc: "/images/fruit01.jpg",
         classname: "水果01",
         price: 12,
-        checkvalue: false
+        checkvalue: false,
+        fgcount:1
       },
       {
         imgsrc: "/images/fruit02.jpg",
         classname: "水果02",
         price: 14,
-        checkvalue: false
+        checkvalue: false,
+        fgcount:1
       },
       {
         imgsrc: "/images/fruit03.jpg",
         classname: "水果03",
         price: 11,
-        checkvalue: false
+        checkvalue: false,
+        fgcount:1
       },
       {
         imgsrc: "/images/bever03.jpg",
         classname: "饮料03",
         price: 18,
-        checkvalue: false
+        checkvalue: false,
+        fgcount:1
       }
 
     ]
@@ -40,20 +43,25 @@ Page({
     //check-group中自定义data-checkindex.
     var chindex = e.currentTarget.dataset.checkindex;
     var ischeck = e.detail.value[0];
-
+    let chvalue = 'classimglist[' + chindex + '].checkvalue';
     if (undefined !== chindex) {
-      var checkprice = this.data.classimglist[chindex].price;
+      var checkprice = this.data.classimglist[chindex].price * this.data.classimglist[chindex].fgcount;
       var samount = this.data.sumamount;
       if (undefined !== ischeck) {
         samount += checkprice;
+        this.setData({
+          [chvalue]: true
+        })
       } else {
         samount -= checkprice;
+        this.setData({
+          [chvalue]: false
+        })
       }
- 
       this.setData({
         sumamount: samount
       })
-      console.log(samount,this.data.allcheckvalue);
+      console.log(samount, this.data.allcheckvalue);
       if (samount == 0) {
         this.setData({
           allcheckvalue: false
@@ -63,35 +71,87 @@ Page({
   },
   // check all item
   allcheckchange: function (e) {
-    var samount = this.data.sumamount;
+    var samount = 0;
     for (var i = 0; i < this.data.classimglist.length; i++) {
       // console.log(this.data.classimglist[i])
       console.log(e.detail);
       let chvalue = 'classimglist[' + i + '].checkvalue';
       if (this.data.classimglist[i].checkvalue === false) {
-        samount += this.data.classimglist[i].price;
+        samount += this.data.classimglist[i].price* this.data.classimglist[i].fgcount;
         console.log(chvalue)
         this.setData({
           [chvalue]: true,
-          sumamount: samount,
-          allcheckvalue:true
+          allcheckvalue: true
         })
       } else {
-        samount -= this.data.classimglist[i].price;
+        samount -= this.data.classimglist[i].price* this.data.classimglist[i].fgcount;
         samount = samount <= 0 ? 0 : samount;
         if (samount === 0 && this.data.allcheckvalue === true) {
           this.setData({
             allcheckvalue: false,
-            allcheckvalue:false
+            allcheckvalue: false
           })
         }
-        console.log(chvalue)
+
         this.setData({
           [chvalue]: false,
           sumamount: samount
         })
       }
     }
+    this.setData({
+      sumamount: samount
+    })
+  },
+  /**减数量**/
+  bindsub: function (e) {
+    // var samount = 0; //钱先不算。
+    var index = e.currentTarget.dataset.subindex;
+    let count = 'classimglist[' + index + '].fgcount';
+    var list = this.data.classimglist[index];
+    if (list.fgcount > 1) {
+      this.setData({
+        [count]: list.fgcount - 1
+      })
+    }
+
+    var samount = 0;this.data.sumamount;
+    for (var i = 0; i < this.data.classimglist.length; i++) {
+
+      if (this.data.classimglist[i].checkvalue === true) {
+        samount += this.data.classimglist[i].price* this.data.classimglist[i].fgcount;
+        samount = samount <= 0 ? 0 : samount;
+        this.setData({
+          sumamount: samount
+        })
+      }
+    }
+    this.setData({
+      sumamount: samount
+    })
+  },
+  bindadd:function(e){
+    //var samount = 0; //钱先不算。
+    var index = e.currentTarget.dataset.addindex;
+    var list = this.data.classimglist[index];
+    let count = 'classimglist[' + index + '].fgcount';
+    console.log(list.fgcount)
+    if (list.fgcount >= 1 && list.fgcount<999) {
+      this.setData({
+        [count]: list.fgcount + 1
+      })
+    }
+    // console.log(list.fgcount,list.price,list.checkvalue )
+    var samount = 0;
+    for (var i = 0; i < this.data.classimglist.length; i++) {
+      let chvalue = 'classimglist[' + i + '].checkvalue';
+      if (this.data.classimglist[i].checkvalue === true) {
+        samount += this.data.classimglist[i].price* this.data.classimglist[i].fgcount;
+      } 
+    }
+    this.setData({
+      sumamount: samount
+    })
   },
   /**
    * 生命周期函数--监听页面加载
